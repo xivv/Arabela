2361e3057e38740c721452c2dfb6ac8ac28ce9d0
xivv <saverio_mancuso@gmx.de>
2016-06-02 14:38:01 +0200

    Add files via upload

---
+++ config/Reader.java
@@ -0,0 +1,74 @@
+package config;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Reader {
+
+	public static File getExternalConfig() {
+		return new File("config.txt");
+
+	}
+	
+	public static String getAlias() {
+
+		String data = "";
+
+		try {
+			Scanner sc = new Scanner(new File("alias.txt"));
+
+			while (sc.hasNextLine()) {
+				data += sc.nextLine() + "\n";
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.err.print("Cant find the alias.txt");
+			e.printStackTrace();
+		}
+
+		return data;
+
+	}
+	
+	public String getColors() {
+
+		String data = "";
+
+		try {
+			Scanner sc = new Scanner(new File("colors.txt"));
+
+			while (sc.hasNextLine()) {
+				data += sc.nextLine() + "\n";
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.err.print("Cant find the colors.txt");
+			e.printStackTrace();
+		}
+
+		return data;
+
+	}
+
+	public String getConfig() {
+
+		String data = "";
+
+		try {
+			Scanner sc = new Scanner(getExternalConfig());
+
+			while (sc.hasNextLine()) {
+				data += sc.nextLine() + "\n";
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.err.print("Cant find the config.txt");
+			e.printStackTrace();
+		}
+
+		return data;
+
+	}
+
+}
